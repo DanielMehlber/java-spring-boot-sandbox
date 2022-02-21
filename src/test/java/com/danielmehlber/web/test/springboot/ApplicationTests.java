00ac5b3ca38312e9ad262340e98eea3a38941ace
Daniel Mehlber <daniel.mehlber@gmail.com>
2022-02-21 21:44:22 +0100

    added both integration testing inside docker container and setup of docker containers for testing (also used in github actions).

--- src/test/java/com/danielmehlber/web/test/springboot/ApplicationTests.java
+++ src/test/java/com/danielmehlber/web/test/springboot/ApplicationTests.java
@@ -1,9 +1,33 @@
 package com.danielmehlber.web.test.springboot;
 
+import com.danielmehlber.web.test.springboot.rest_api.PersonAPI;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
+
+    @Autowired
+    private MockMvc client;
+
+    @Test
+    public void testUnknownPersonFetch() throws Exception {
+        client.perform(get("/person/id/9999")).andExpect(status().isNoContent());
+    }
 
 }
